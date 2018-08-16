d8f28df474e6f0e679cb175bcce387134995e3f9
ct <1528907295@qq.com>
2018-08-15 21:35:49 -0700

    modify testframe.c: finish test framework.

--- seqlist/testframe.c
+++ seqlist/testframe.c
@@ -2,36 +2,51 @@
 #include<stdlib.h>
 #include "seqlist.h"
 
-SeqList* SeqList_Create(int capacity){
+typedef struct _Teacher{
+	int age;
+	char name[64];
+}Teacher;
+
+
+
+
+int main(){
+	int ret=0;
+	int i=0;
 	SeqList* list=NULL;
-	list=(SeqList *)malloc(sizeof(SeqList *));
+
+	Teacher t1,t2,t3,t4,t5;
+	t1.age=21;
+	t2.age=22;
+	t3.age=23;
+	t4.age=24;
+	t5.age=25;
+
+	list=SeqList_Create(10);
 	if(list==NULL){
-		printf("func SeqList_Create error\n");
-		return NULL;
+		printf("create fail\n");
+		return -1;
+	}
+	
+	ret=SeqList_Insert(list,(SeqListNode*)&t1,0);
+	ret=SeqList_Insert(list,(SeqListNode*)&t2,0);
+	ret=SeqList_Insert(list,(SeqListNode*)&t3,0);
+	ret=SeqList_Insert(list,(SeqListNode*)&t4,0);
+	ret=SeqList_Insert(list,(SeqListNode*)&t5,0);
+
+	for(i=0;i<SeqList_Length(list);i++){
+		Teacher* tmp=(Teacher*)SeqList_Get(list,i);
+		if(tmp==NULL){
+			printf("SeqList_Get fail\n");
+			return -1;
+		}
+		printf("tmp->age: %d\n",tmp->age);
 	}	
-	return list;
-}
 
-void SeqList_Destory(SeqList* list){
-	return ;
-}
-
-void SeqList_Clear(SeqList* list){
-	return ;
-}
-
-int SeqList_Length(SeqList* list){
+	while(SeqList_Length(list)>0){
+		SeqList_Delete(list,0);
+	}
 	return 0;
 }
 
-int SeqList_Insert(SeqList* list,SeqListNode* node,int pos){
-	return 0;
-}
 
-SeqListNode* SeqList_Get(SeqList* list,int pos){
-	return NULL;
-}
-
-SeqListNode* SeqList_Delete(SeqList* list,int pos){
-	return NULL;
-}
